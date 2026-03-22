864793ce8fc4cfc901ac1a3c5a11e43af004b43b
Mona Lisa <anthonyrichardo080@gmail.com>
2026-03-22 14:12:27 +0100

    fixed:grid-placement and alignment

--- script.js
+++ script.js
@@ -1,39 +1,74 @@
-const unitContainer = document.querySelector('.unit-selector-container');
-const dropdown = unitContainer.querySelector('.dropdown');
-const dropdownMenu = unitContainer.querySelector('.dropdown-menu');
-const daysDropdownMenu = document.querySelector('.days-dropdown-menu')
+const dropdown = document.querySelector('.dropdown');
+const dropdownMenu = document.querySelector('.dropdown-menu');
 const daysDropdown = document.querySelector('.days-dropdown');
-const dropdownIcon = unitContainer.querySelectorAll('.dropdown-icon');
+const dropdownIcon = document.querySelector('.dropdown-icon');
+const daysdropdownMenu = document.querySelector('.days-dropdown-menu');
+const daysdropdownIcon = document.querySelector('.days-dropdown-icon');
+
+// this is a function to call the open weather API
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // a function for closing and opening the dropdown menu
 function itemsDropdown(){
     // this is just to make the dropdown menu visibles
     dropdownMenu.classList.toggle('opacity-0');
     dropdownMenu.classList.toggle('-translate-y-2');
-    dropdownIcon.forEach(icon => {
-        icon.classList.toggle('rotate-180');
-    }) || dropdownIcon.classList.toggle('rotate-180')
+    dropdownIcon.classList.toggle('rotate-180');
 }
 
 // this is to close the dropdown when its clicked outside of it.
 function closeitemsDropdown(e){
     if(!dropdown.contains(e.target) && !dropdownMenu.contains(e.target)){
-       dropdownMenu.classList.add('opacity-0');
-    dropdownMenu.classList.add('-translate-y-2');
-    dropdownIcon.forEach(icon => {
-        icon.classList.remove('rotate-180');
-    })
-    }
+        dropdownMenu.classList.add('opacity-0');
+        dropdownMenu.classList.add('-translate-y-2');
+        dropdownIcon.classList.remove('rotate-180')
+    };
+
+    //close the day-drop if statement because why not.
+
+    if(!daysDropdown.contains(e.target) && !daysdropdownMenu.contains(e.target)){
+         daysdropdownMenu.classList.add('opacity-0');
+        daysdropdownMenu.classList.add('-translate-y-2');
+        daysdropdownIcon.classList.remove('rotate-180');
+    };
 }
 
-//this is a function for the daysdropdownMenu, reminder, richard: this is a temporary just to make it work till i find a solution.
-function dropdownDaysMenu(){
-    daysDropdownMenu.classList.toggle('opacity-0');
-    daysDropdownMenu.classList.toggle('-translate-y-2');
+// this is a function for the days-dropdown element, this is just a temporary solution for this.
+function toggleDayDropdown(){
+    daysdropdownMenu.classList.toggle('opacity-0');
+    daysdropdownMenu.classList.toggle('-translate-y-2');
+    daysdropdownIcon.classList.toggle('rotate-180');
 }
-
 
 document.addEventListener('click', closeitemsDropdown);
 dropdown.addEventListener('click', itemsDropdown);
-//this is for the days dropdown, this is to reminder to find a better way around it but let's do something temporary
-daysDropdown.addEventListener('click', dropdownDaysMenu);
+daysDropdown.addEventListener('click', toggleDayDropdown);
